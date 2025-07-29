ba2f3c3d8132f48703c309c207a8a6a477c8ede0
Snahasis-Barat <74712869+Snahasis-Barat@users.noreply.github.com>
2025-07-30 00:21:30 +0530

    Update const.js

--- src/utils/const.js
+++ src/utils/const.js
@@ -6,8 +6,8 @@ export const search_movies_url="https://api.themoviedb.org/3/search/movie?query=
 export const options = {
   method: 'GET',
   headers: {
-    accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4MmVmMWRmZjg2N2VjOGY5ZjQ1ZTgxN2VhNDZiNTA4ZCIsIm5iZiI6MTc1MzAzNzIzNi4xNDMwMDAxLCJzdWIiOiI2ODdkMzliNGEyMzE0NDFmYjUxNjlmMDYiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.gc9uTspHkOn7_hCzhowqukDNqdGBwh5lENzMq-CBcPQ'
+    accept: 'application/json'
+    
   }
 };
 
